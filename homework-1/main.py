9700791168a0245a34bdeab51d8c8aca76441769
Max Slidzen <slidzon94@gmail.com>
2023-06-27 23:46:27 +0300

    Refactored code. Created function to fill tables using "sample"

--- homework-1/main.py
+++ homework-1/main.py
@@ -6,42 +6,51 @@ import os
 # Персональный пароль для работы с postgres
 PASSWORD = os.getenv('psql_password')
 
+# Переменные для пути к файлам csv
+customers_path = 'north_data/customers_data.csv'
+employees_path = 'north_data/employees_data.csv'
+orders_path = 'north_data/orders_data.csv'
+
+
+def write_table(path: str, table_name: str, cursor: 'psycopg2.connect().cursor()') -> None:
+    """
+    Функция для записи данных в таблицы базы данных
+
+    :param path: Путь к файлу с данными
+    :param table_name: Название таблицы
+    :param cursor: Экземпляр класса cursor для работы с базами данных
+    """
+
+    with open(path) as file:
+        reader = csv.DictReader(file)
+
+        # Создание шаблона для команды записи строки в таблицу
+        len_pattern = len(reader.fieldnames)
+        sample_list = []
+        for i in range(len_pattern):
+            sample_list.append('%s')
+        sample = ', '.join(sample_list)
+
+        # Запись данных по шаблону
+        for row in reader:
+            values = row.values()
+
+            cursor.execute(f'INSERT INTO {table_name} VALUES ({sample})',
+                           tuple(values))
+
 
 def main():
     """
     Основная функция работы программы
     """
-    # Открытие базы данных
     conn = psycopg2.connect(database='north', user='postgres', password=PASSWORD)
-
     try:
         with conn:
-            # Создание экземпляра курсора для работы
             with conn.cursor() as cur:
+                write_table(customers_path, 'customers', cur)
+                write_table(employees_path, 'employees', cur)
+                write_table(orders_path, 'orders', cur)
 
-                # Заполнение таблицы customers из csv файла
-                with open('north_data/customers_data.csv', encoding='utf-8') as file:
-                    reader = csv.DictReader(file)
-                    for row in reader:
-                        cur.execute('INSERT INTO customers VALUES (%s, %s, %s)',
-                                    (row['customer_id'], row['company_name'], row['contact_name']))
-
-                # Заполнение таблицы employees из csv файла
-                with open('north_data/employees_data.csv', encoding='utf-8') as file:
-                    reader = csv.DictReader(file)
-                    for row in reader:
-                        cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)',
-                                    (row['employee_id'], row['first_name'], row['last_name'],
-                                     row['title'], row['birth_date'], row['notes']))
-
-                # Заполнение таблицы orders из csv файла
-                with open('north_data/orders_data.csv', encoding='utf-8') as file:
-                    reader = csv.DictReader(file)
-                    for row in reader:
-                        cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)',
-                                    (row['order_id'], row['customer_id'], row['employee_id'],
-                                     row['order_date'], row['ship_city']))
-    # Закрытие базы данных
     finally:
         conn.close()
 
